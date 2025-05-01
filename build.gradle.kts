584967b79486642ae44e615da32fd1cfba1ecab2
Omar Aly <omarplaystation8888@gmail.com>
2025-05-01 21:15:39 +0300

    - for learning

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,11 @@ plugins {
 group = "com.flutter-structures"
 version = "1.0.2-SNAPSHOT"
 
+
+kotlin {
+    jvmToolchain(17)
+}
+
 repositories {
     mavenCentral()
 }
@@ -18,7 +23,7 @@ apply(plugin = "org.jetbrains.intellij")
 intellij {
     version.set("2023.1.5")
     type.set("IC") // Target IDE Platform
-    plugins.set(listOf(/* Plugin Dependencies */))
+    plugins.set(listOf("terminal"))
 }
 
 tasks {
